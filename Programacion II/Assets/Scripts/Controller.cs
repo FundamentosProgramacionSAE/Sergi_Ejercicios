6bd18a74d6a75ec536867c77d673f5e527364c90
Sergist71988 <96082459+Sergist71988@users.noreply.github.com>
2022-05-26 19:15:11 +0200

    Salto

--- Programacion II/Assets/Scripts/Controller.cs
+++ Programacion II/Assets/Scripts/Controller.cs
@@ -8,30 +8,56 @@ public class Controller : MonoBehaviour
     public float SpeedWalk;
     public float SpeedRun;
     public Animator Animator;
+    public float JumpForce;
+    public float Gravity;
+    
 
     private float _velocity;
+    
 
     void Start()
     {
-        
-    }
 
-    
+    }
     void Update()
     {
         float xAxis = Input.GetAxis("Horizontal");
         float zAxis = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(xAxis, 0f, zAxis);
 
-        _velocity = Input.GetKey(KeyCode.LeftShift) ? SpeedRun : SpeedWalk;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _velocity = SpeedRun;
+        }
+        else
+        {
+            _velocity = SpeedWalk;
+        }
 
         CharacterController.Move(movement * _velocity * Time.deltaTime);
 
-        Animator.SetFloat("Horizontal", xAxis);
-        Animator.SetFloat("Vertical", zAxis);
+        if (_velocity == SpeedWalk)
+        {
+            Animator.SetFloat("Horizontal", xAxis, 0.1f, Time.deltaTime);
+            Animator.SetFloat("Vertical", zAxis, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            Animator.SetFloat("Vertical", 2, 0.1f, Time.deltaTime);
+        }
 
-        
+        if (CharacterController.isGrounded)
+        {            
+            movement *= _velocity;
 
+            if (Input.GetButton("Jump"))
+            {
+                movement.y = JumpForce;
+            }     
+        }
+        movement.y -= Gravity * Time.deltaTime;
 
+        CharacterController.Move(movement * Time.deltaTime);
+        Debug.Log(CharacterController.isGrounded);
     }
 }
