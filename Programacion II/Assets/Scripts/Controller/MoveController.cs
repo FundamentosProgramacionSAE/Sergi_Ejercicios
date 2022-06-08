069752f66547aa3e56076b60e340e38842eb4136
David Garcia Mateo <GS.projectgames@gmail.com>
2022-06-08 10:50:23 +0200

    UIgame Finish

--- Programacion II/Assets/Scripts/Controller/MoveController.cs
+++ Programacion II/Assets/Scripts/Controller/MoveController.cs
@@ -6,6 +6,11 @@ public class MoveController : MonoBehaviour
 {
     public GameObject Player;
     public float Fuerza;
+    public GameObject LoserScreen;
+    public GameObject WinnerScreen;
+    public GameObject WinGame;
+    public List <GameObject> Spheres;
+    public bool CanYouWin;
 
     private Rigidbody _rigidbody;
     private Vector3 _movementDirection;
@@ -24,6 +29,51 @@ public class MoveController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+    }
+
+    public void ClickAndRotatePositive()
+    {
+        Player.transform.Rotate(0, 90f, 0);
+    }
+    public void ClickandRotateNegative()
+    {
+        Player.transform.Rotate(0, -90f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            LoserScreen.SetActive(true);  
+        }
+        if (other.CompareTag("Pelota"))
+        {
+            Spheres.Remove(other.gameObject);
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("Win"))
+        {
+            WinnerScreen.SetActive(true);
+        }
+    }
+    public bool ArrayEmpty()
+    {
+        if (Spheres == null || Spheres.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    private void Update()
+    {
+        CanYouWin = ArrayEmpty();
+        if (CanYouWin == true)
+        {
+            WinGame.SetActive(true);
+        }
     }
 
 
